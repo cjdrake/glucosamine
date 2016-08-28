1a8814622be9b9bd1b06ecc03acdae7e493e4675
Chris Drake <cjdrake@gmail.com>
2016-08-27 23:04:10 -0700

    Get rid of some unused functions

--- src/utils/ParseUtils.h
+++ src/utils/ParseUtils.h
@@ -18,91 +18,12 @@ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
 OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 **************************************************************************************************/
 
+
 #ifndef Glucose_ParseUtils_h
 #define Glucose_ParseUtils_h
 
-#include <stdlib.h>
-#include <stdio.h>
-#include <math.h>
-
 
 namespace Glucose {
-
-
-static inline bool isEof(const char*   in)
-{
-    return *in == '\0';
-}
-
-//-------------------------------------------------------------------------------------------------
-// Generic parse functions parametrized over the input-stream type.
-
-
-template<class B>
-static void skipWhitespace(B& in)
-{
-    while ((*in >= 9 && *in <= 13) || *in == 32)
-        ++in;
-}
-
-
-template<class B>
-static void skipLine(B& in)
-{
-    for (;;) {
-        if (isEof(in)) return;
-        if (*in == '\n') {
-            ++in;
-            return;
-        }
-        ++in;
-    }
-}
-
-template<class B>
-static double parseDouble(B& in)   // only in the form X.XXXXXe-XX
-{
-    bool    neg= false;
-    double accu = 0.0;
-    double currentExponent = 1;
-    int exponent;
-
-    skipWhitespace(in);
-    if(*in == EOF) return 0;
-    if      (*in == '-') neg = true, ++in;
-    else if (*in == '+') ++in;
-    if (*in < '1' || *in > '9') printf("PARSE ERROR! Unexpected char: %c\n", *in), exit(3);
-    accu = (double)(*in - '0');
-    ++in;
-    if (*in != '.') printf("PARSE ERROR! Unexpected char: %c\n", *in),exit(3);
-    ++in; // skip dot
-    currentExponent = 0.1;
-    while (*in >= '0' && *in <= '9')
-        accu = accu + currentExponent * ((double)(*in - '0')),
-        currentExponent /= 10,
-                           ++in;
-    if (*in != 'e') printf("PARSE ERROR! Unexpected char: %c\n", *in),exit(3);
-    ++in; // skip dot
-    exponent = parseInt(in); // read exponent
-    accu *= pow(10,exponent);
-    return neg ? -accu:accu;
-}
-
-
-template<class B>
-static int parseInt(B& in)
-{
-    int     val = 0;
-    bool    neg = false;
-    skipWhitespace(in);
-    if      (*in == '-') neg = true, ++in;
-    else if (*in == '+') ++in;
-    if (*in < '0' || *in > '9') fprintf(stderr, "PARSE ERROR! Unexpected char: %c\n", *in), exit(3);
-    while (*in >= '0' && *in <= '9')
-        val = val*10 + (*in - '0'),
-        ++in;
-    return neg ? -val : val;
-}
 
 
 // String matching: in case of a match the input iterator will be advanced the corresponding
@@ -120,18 +41,8 @@ static bool match(B& in, const char* str)
     return true;
 }
 
-// String matching: consumes characters eagerly, but does not require random access iterator.
-template<class B>
-static bool eagerMatch(B& in, const char* str)
-{
-    for (; *str != '\0'; ++str, ++in)
-        if (*str != *in)
-            return false;
-    return true;
-}
+
+}  // namespace Glucose
 
 
-//=================================================================================================
-}
-
-#endif
+#endif  // Glucose_ParseUtils_h
