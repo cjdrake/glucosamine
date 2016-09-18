ed622ee9b992e6c994d2c604129525e11e0752d5
Chris Drake <cjdrake@gmail.com>
2016-09-17 21:32:13 -0700

    Get rid of some unused algorithm functions

--- src/mtl/Alg.h
+++ src/mtl/Alg.h
@@ -25,13 +25,6 @@ OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWA
 namespace Glucose {
 
 
-//=================================================================================================
-// Useful functions on vector-like types:
-
-//=================================================================================================
-// Removing and searching for elements:
-//
-
 template<class V, class T>
 static inline void remove(V& ts, const T& t)
 {
@@ -40,45 +33,6 @@ static inline void remove(V& ts, const T& t)
     assert(j < ts.size());
     for (; j < ts.size()-1; j++) ts[j] = ts[j+1];
     ts.pop();
-}
-
-
-template<class V, class T>
-static inline bool find(V& ts, const T& t)
-{
-    int j = 0;
-    for (; j < ts.size() && ts[j] != t; j++);
-    return j < ts.size();
-}
-
-
-//=================================================================================================
-// Copying vectors with support for nested vector types:
-//
-
-// Base case:
-template<class T>
-static inline void copy(const T& from, T& to)
-{
-    to = from;
-}
-
-// Recursive case:
-template<class T>
-static inline void copy(const vec<T>& from, vec<T>& to, bool append = false)
-{
-    if (!append)
-        to.clear();
-    for (int i = 0; i < from.size(); i++) {
-        to.push();
-        copy(from[i], to.last());
-    }
-}
-
-template<class T>
-static inline void append(const vec<T>& from, vec<T>& to)
-{
-    copy(from, to, true);
 }
 
 
